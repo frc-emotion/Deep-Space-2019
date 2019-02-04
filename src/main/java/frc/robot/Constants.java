282da9268cd0febbc6a512b781f166349da178d2
ryanchaiyakul <ryanchaiyakul@gmail.com>
2019-02-03 19:19:41 -0800

    added a ';' so it doesn't complain

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,7 +8,7 @@ public class Constants{
     public static final double ENCODER_REV = 42; //number of ticks per motor rev
     
     // DriveTrain constants
-    public static final int DTRAIN_PORT_1 = 0, DTRAIN_PORT_2 = 0, DTRAIN_PORT_3 = 0, // Ports for left side of driveTrain
+    public static final int DTRAIN_PORT_1 = 0, DTRAIN_PORT_2 = 0, DTRAIN_PORT_3 = 0; // Ports for left side of driveTrain
     public static final int DTRAIN_PORT_4 = 0, DTRAIN_PORT_5 = 0, DTRAIN_PORT_6 = 0; // Ports for right side of driveTrain
     public static final double CURVE = 1.5; // for tankDrive as a param
     public static final double POWER = 1; // for tankDrive as a param
