83d3ae46014bdccdb7d29f8297f610bb16cf5d8a
Gokul Swami <gokulswamilive@gmail.com>
2019-03-06 22:49:03 -0800

    removed unused dashboard stuff and added some vars

--- src/main/java/frc/robot/RobotFlipper.java
+++ src/main/java/frc/robot/RobotFlipper.java
@@ -1,7 +1,6 @@
 package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
@@ -48,7 +47,7 @@ public class RobotFlipper {
         // drive power (max 1)
         climbPower = 0.5;
         // deadzone exponent
-        climbExponent = 1.1;
+        climbExponent = 2;
         disableScrew = false;
         screwTalon = new WPI_TalonSRX(Constants.SCREW_TALON_CID);
 
@@ -102,8 +101,7 @@ public class RobotFlipper {
             disableScrew = true;
             Robot.operatorController.setRumble(RumbleType.kRightRumble, 0.5);
             Robot.operatorController.setRumble(RumbleType.kLeftRumble, 0.5);
-        }
-        else{
+        } else {
             Robot.operatorController.setRumble(RumbleType.kRightRumble, 0);
             Robot.operatorController.setRumble(RumbleType.kLeftRumble, 0);
         }
@@ -114,7 +112,8 @@ public class RobotFlipper {
      */
     void runClimber() {
         int dirConstant = 1; // for easy direction change
-        // double stickInput = Robot.climbController.getRawAxis(Constants.CLIMBER_CONTROLLER_AXIS);
+        // double stickInput =
+        // Robot.climbController.getRawAxis(Constants.CLIMBER_CONTROLLER_AXIS);
         climbPower = SmartDashboard.getNumber("Climb Power", 0.5);
         // if (stickInput > 0.2) {
         // constant = 1;
@@ -126,45 +125,44 @@ public class RobotFlipper {
         // int pov = Robot.operatorController.getPOV();
         // switch (pov) {
         // case 0:
-        //     climbDrive.tankDrive(speed, speed);
-        //     break;
+        // climbDrive.tankDrive(speed, speed);
+        // break;
         // case 180:
-        //     climbDrive.tankDrive(-speed, -speed);
-        //     break;
+        // climbDrive.tankDrive(-speed, -speed);
+        // break;
         // case -1:
-        //      climbDrive.tankDrive(0, 0);
+        // climbDrive.tankDrive(0, 0);
         // default:
-        //     climbDrive.tankDrive(0, 0);
-        //     break;
+        // climbDrive.tankDrive(0, 0);
+        // break;
         // }
 
         // if (Math.abs(stickInput) > 0.2) {
-        //     // set the motor (which sets the other automatically) to run based on joystick
-        //     // climbSparkA.set(constantA * climbPower * stickInput);
-        //     // climbSparkB.set(constantB * climbPower * stickInput);
+        // // set the motor (which sets the other automatically) to run based on
+        // joystick
+        // // climbSparkA.set(constantA * climbPower * stickInput);
+        // // climbSparkB.set(constantB * climbPower * stickInput);
 
-        //     double speed = constantB * climbPower * stickInput;
+        // double speed = constantB * climbPower * stickInput;
 
-        //     climbDrive.tankDrive(speed, speed, true);
+        // climbDrive.tankDrive(speed, speed, true);
         // } else {
-        //     climbDrive.tankDrive(0, 0);
+        // climbDrive.tankDrive(0, 0);
         // }
-        if(Robot.operatorController.getBumper(Hand.kLeft)){
+        if (Robot.operatorController.getBumper(Hand.kLeft)) {
             double stickInput = Robot.operatorController.getRawAxis(Constants.CLIMBER_CONTROLLER_AXIS);
             if (stickInput > 0.2) {
                 dirConstant = 1;
-            } 
-            else if (stickInput < -0.2) {
+            } else if (stickInput < -0.2) {
                 dirConstant = -1;
             }
-            if(Math.abs(stickInput) > 0.2){
-                climbDrive.tankDrive(dirConstant*climbPower* Math.pow(Math.abs(stickInput), 2), dirConstant*climbPower*Math.pow(Math.abs(stickInput), 2));
-            }
-            else{
+            if (Math.abs(stickInput) > 0.2) {
+                climbDrive.tankDrive(dirConstant * climbPower * Math.pow(Math.abs(stickInput), climbExponent),
+                        dirConstant * climbPower * Math.pow(Math.abs(stickInput), climbExponent));
+            } else {
                 climbDrive.tankDrive(0, 0);
             }
-        }
-        else{
+        } else {
             climbDrive.tankDrive(0, 0);
         }
     }
@@ -173,7 +171,7 @@ public class RobotFlipper {
      * Initializes shuffleboard values and drop downs
      */
     void initShuffleBoard() {
-        SmartDashboard.putNumber("Climb Power", climbPower);
+        // SmartDashboard.putNumber("Climb Power", climbPower);
         // SmartDashboard.putNumber("Climb Exponent", climbExponent);
     }
 
@@ -181,10 +179,10 @@ public class RobotFlipper {
      * Keeps updating shuffleboard values
      */
     void workShuffleBoard() {
-        SmartDashboard.putNumber("Screw Current", screwTalon.getOutputCurrent());
+        // SmartDashboard.putNumber("Screw Current", screwTalon.getOutputCurrent());
         SmartDashboard.putBoolean("Screw", !disableScrew);
-        SmartDashboard.putNumber("Climber Encoder", climbEncoder.getPosition());
-        SmartDashboard.putNumber("Climb A Current", climbSparkA.getOutputCurrent());
-        SmartDashboard.putNumber("Climb B Current", climbSparkB.getOutputCurrent());
+        // SmartDashboard.putNumber("Climber Encoder", climbEncoder.getPosition());
+        // SmartDashboard.putNumber("Climb A Current", climbSparkA.getOutputCurrent());
+        // SmartDashboard.putNumber("Climb B Current", climbSparkB.getOutputCurrent());
     }
 }
